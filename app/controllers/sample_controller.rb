6cc37e8dbc4bc3facad029bc471924d06ddad8d7
mustanghippie69 <mustanghippie69@gmail.com>
2018-07-23 17:33:18 +0700

    Add get random image from google photo

--- app/controllers/sample_controller.rb
+++ app/controllers/sample_controller.rb
@@ -4,13 +4,19 @@ class SampleController < ApplicationController
   require 'redis'
   require 'weather_forecast'
   require 'google_api_drive'
+  require 'google_api_photo'
+  require 'picasa'
 
   def index
   	logger = Logger.new(STDERR)
     calendar = Calendar.new
     weatherForecast = WeatherForecast.new
     gDrive = GoogleDrive.new
-    @result = gDrive.get_drive
+    gPhoto = GooglePhoto.new
+    @result = gPhoto.get_random_photo
+    
+    #client.album.list
+    #@result = gDrive.get_drive
     #@result = calendar.get_schedule
     #@result = weatherForecast.get_weather
   end
