7c1031e0abb4878e42a713823c1891652a68157d
mustanghippie69 <mustanghippie69@gmail.com>
2018-08-06 21:36:42 +0700

    本番環境もユーザ情報をpostgresから取得するように変更

--- lib/linebot_push_message.rb
+++ lib/linebot_push_message.rb
@@ -13,11 +13,9 @@ class LinebotPushMessage
       users = User.select(:uid)
       setting_channel = YAML.load_file("setting_line_channel.yaml")
       setting_channel = {channel_secret: setting_channel['LINE_CHANNEL_SECRET'], channel_token: setting_channel['LINE_CHANNEL_TOKEN']}
-
     else
       setting_channel = {channel_secret: ENV['LINE_CHANNEL_SECRET'], channel_token: ENV['LINE_CHANNEL_TOKEN']}
-      redis = Redis.new(url: ENV['REDIS_URL'])
-      users = JSON.parse(ENV['linebot_user_list'])
+      users = User.select(:uid)
     end
 
     client = Line::Bot::Client.new { |config|
